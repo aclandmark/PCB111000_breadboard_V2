085726523a3d2ce16d8a32297a9b1247d39ef68d
aclandmark <mark.osborne15@btinternet.com>
2026-03-05 17:14:50 +0000

    Create UNO_AVR_programmer.h

---
+++ Bootloader_for_168/UNO_programs_168/UNO_AVR_programmer.h
@@ -0,0 +1,12 @@
+
+#include <avr/wdt.h>
+
+#include "Resources_UNO_AVR_programmer\UNO_AVR_Programmer.h"
+
+#include "Resources_UNO_AVR_programmer\UNO_AVR_Programmer_HW_subs.c"
+#include "Resources_UNO_AVR_programmer\Basic_IO_and_Timer_subs.c"
+#include "Resources_UNO_AVR_programmer\ASKII_subroutines.c"
+#include "Resources_UNO_AVR_programmer\Hex_Programmer_subs.c"
+#include "Resources_UNO_AVR_programmer\EEPROM_programmer_sub.c"
+
+#include "Resources_UNO_AVR_programmer\Hex_Programmer.c"
