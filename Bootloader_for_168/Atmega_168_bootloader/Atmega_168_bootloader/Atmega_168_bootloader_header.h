e5bfa2c21cc970c9eb63e769b74b99670a5e4736
aclandmark <mark.osborne15@btinternet.com>
2026-03-05 17:12:41 +0000

    Create Atmega_168_bootloader_header.h

---
+++ Bootloader_for_168/Atmega_168_bootloader/Atmega_168_bootloader/Atmega_168_bootloader_header.h
@@ -0,0 +1,13 @@
+
+#define F_CPU 8000000UL
+//#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/interrupt.h>
+//#include <avr/wdt.h>
+//#include <avr/eeprom.h>
+//#include <util/delay.h>
+
+
+#include "../Programmer_Resources\Hex_txt_bootloader.h"
+#include "../Programmer_Resources\Hex_txt_bootloader_SW_subs.c"
+#include "../Programmer_Resources\Basic_IO_subroutines.c"
