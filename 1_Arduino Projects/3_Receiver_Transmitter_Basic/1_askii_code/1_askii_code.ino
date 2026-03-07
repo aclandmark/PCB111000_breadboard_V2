6b2046fde65c7ca893180379e31315ed68134d3c
aclandmark <mark.osborne15@btinternet.com>
2026-03-07 13:35:10 +0000

    Create 1_askii_code.ino

---
+++ 1_Arduino Projects/3_Receiver_Transmitter_Basic/1_askii_code/1_askii_code.ino
@@ -0,0 +1,57 @@
+
+//10 projects that explore
+          //Text strings
+          //Askii characters
+          //Downloading and printing text files
+          //Storing strings in working memory
+          //Upper to lower case conversion
+          //Printing .hex files with text in clear
+
+
+
+
+#include "Receiver_Transmitter_header.h"
+#include "Local_subroutines.c"
+
+
+//Ten simple examples to experiment with.
+
+int main (void)                //Example 6
+  { setup_HW;         //Exploring program memory
+  char *mem_add;
+ wdt_enable(WDTO_120MS);
+ //while(switch_2_down)wdr();
+  
+  
+  String_to_PC_Local("\0Null string\r\n");
+  String_to_PC_Local("Defining and using text strings\r\n\r\n");
+  const char *message_1 = "Hello world\r\n";
+  const char *message_2 = "Sending text to a PC\r\n";
+  const char message_3[] = "Writing C programs and\r\n";
+  const char message_4[] = "Uploading them to a device\r\n\r\n";
+  String_to_PC_Local(message_1);
+  String_to_PC_Local(message_2);
+  String_to_PC_Local(message_3);
+  String_to_PC_Local(message_4);
+  while(switch_3_up)wdr();
+
+ String_to_PC_Local("Print out of program memory\r\n\r\n");
+
+mem_add = (char*)256;
+
+for(int m = 0; m <=185; m++){
+  if ((*(mem_add + m))== '\0')Hex_and_Int_to_PC_Basic (10,256+m);
+ else Char_to_PC_Basic(*(mem_add + m));}
+
+  newline_Basic();newline_Basic();
+  
+  while(switch_3_down)wdr();
+ //while(switch_3_up)wdr();
+  
+  return 1;}
+
+
+
+
+
+//**************************************************************************************************************************************
