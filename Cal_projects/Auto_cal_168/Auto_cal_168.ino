5372d8b2a5305ebc4add456a500f2eedb198c49d
aclandmark <mark.osborne15@btinternet.com>
2026-03-16 17:52:55 +0000

    Update Auto_cal_168.ino

--- Cal_projects/Auto_cal_168/Auto_cal_168.ino
+++ Cal_projects/Auto_cal_168/Auto_cal_168.ino
@@ -57,19 +57,19 @@ Timer_T0_sub(T0_delay_5ms);                 //Allows printing to finish before O
     TIMSK1 &= (~(1 << TOIE1));
     Timer_T0_10mS_delay_x_m(10);
     newline();
-    sendLongNum(16,OSCCAL_approx); sendChar('\t');
+    sendLongNum(10,OSCCAL_approx); sendChar('\t');
     sendLongNum(10,error_initial); sendString("\tInitial result\r\n");
     
-    sendLongNum(16,OSCCAL_WV); sendChar('\t');
+    sendLongNum(10,OSCCAL_WV); sendChar('\t');
     sendLongNum(10,error); sendString("\tFinal result\r\n");
     
     eeprom_write_byte((uint8_t*)0x1FF, OSCCAL_WV); 
   eeprom_write_byte((uint8_t*)0x1FE, OSCCAL_WV);
   
   sendString("\r\nValues saved to EEPROM\t");       //Echo values back from the EEPROM
-  sendLongNum(16,eeprom_read_byte((uint8_t*)0x1FF));
+  sendLongNum(10,eeprom_read_byte((uint8_t*)0x1FF));
   sendString ("    ");
-  sendLongNum(16,eeprom_read_byte((uint8_t*)0x1FE));
+  sendLongNum(10,eeprom_read_byte((uint8_t*)0x1FE));
   sendString("\r\nAK to repeat\r\n");
   waitforkeypress();SW_reset;}    //keypress to repeat cal process
     //keypress to repeat cal process
