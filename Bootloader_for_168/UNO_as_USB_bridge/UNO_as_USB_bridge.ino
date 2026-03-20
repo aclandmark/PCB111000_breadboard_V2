342c7fce6b93dba3388b721150690ce70a75d17e
aclandmark <mark.osborne15@btinternet.com>
2026-03-20 21:28:40 +0000

    Create UNO_as_USB_bridge.ino

---
+++ Bootloader_for_168/UNO_as_USB_bridge/UNO_as_USB_bridge.ino
@@ -0,0 +1,18 @@
+
+
+
+
+#include "UNO_AVR_programmer.h"
+#define Version "UNO_programmes_168_prototype\r\n" 
+
+int main (void){ 
+
+setup_328_HW;                                                         //Reduces clock to 8MHz"
+
+Reset_H;
+
+return 1;}
+
+
+
+/***************************************************************************************************************************************************/
