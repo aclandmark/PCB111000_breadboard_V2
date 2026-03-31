e97335de5c8aa6655352f0daec534a28c384411b
aclandmark <mark.osborne15@btinternet.com>
2026-03-31 21:13:31 +0100

    Update 8_Digit_display.ino

--- 1_Arduino Projects/4_Display_Driver_Basic/8_Digit_display/8_Digit_display.ino
+++ 1_Arduino Projects/4_Display_Driver_Basic/8_Digit_display/8_Digit_display.ino
@@ -1,7 +1,4 @@
 
-//Defining digits in terms of their segments
-//so that numbers can simply be entered at the keyboard
-
 
 #include "8_Digit_header.h"
 #include "display_header.h"
@@ -9,16 +6,15 @@
 
 int main (void){
 
-char   digit='0';
+char   digit;
 int digit_num=0;            
 int string_counter=0;
 int letter_counter=0;
 const char* string_ptr = 0;
-char num_string[9] = "86421357";
+char num_string[9];// = "86421357";
 
 
 setup_HW;               
-//digit_1_LH_on;
 
 if(MCUSR & (1 << PORF))
 {User_prompt_Basic;
@@ -27,38 +23,34 @@ MCUSR = 0;Clear_segments;}
 
 if(!(eeprom_read_byte((uint8_t*)0x1FA)))
 {eeprom_write_byte((uint8_t*)0x1FA, 0xFF);
-String_to_PC_Basic("\r\nSend digits?");}
+String_to_PC_Basic("\r\nSend integer?");}
 
 else 
 
 String_to_PC_Basic("\r\nAgain");
-Clear_segments;
 
-while(1){
-String_to_PC_Basic("1 to 8?");
-switch (waitforkeypress_Basic()){
-case '1':  digit_4_RH_on; break;
-case '2':  digit_3_RH_on; break;
-case '3':  digit_2_RH_on; break;
-case '4':  digit_1_RH_on; break;
-case '5':  digit_4_LH_on; break;
-case '6':  digit_3_LH_on; break;
-case '7':  digit_2_LH_on; break;
-case '8':  digit_1_LH_on; break;
-}
+Int_from_PC_Basic(num_string);
+
+while(1){digit_num=0;
+
+do{
+  Clear_digits;
+  Clear_segments;
+digit = num_string[digit_num];
+
+switch (digit_num + 1){
+case 1:  digit_4_RH_on; break;
+case 2:  digit_3_RH_on; break;
+case 3:  digit_2_RH_on; break;
+case 4:  digit_1_RH_on; break;
+case 5:  digit_4_LH_on; break;
+case 6:  digit_3_LH_on; break;
+case 7:  digit_2_LH_on; break;
+case 8:  digit_1_LH_on; break;}
 
   
-digit_num = 0;                                  //First digit on display
-
-do{                                             //start of "do{}while();" loop
-//while(!(isCharavailable_Basic(1)))wdr(); 
-Clear_segments;//digit_4_RH_on;
-//digit = Char_from_PC_Basic();                   //user enters digit (0 to 9) at the PC keyboard
-digit = num_string[digit_num];
-switch(digit){                                  //The appropriate address is loaded into location 
-                                                //"string_pointer"
-case '0': string_ptr = zero; break;             //The address of array zero is loaded into 
-                                                //location "string_ptr"
+switch(digit){ 
+case '0': string_ptr = zero; break;
 case '1': string_ptr = one; break;
 case '2': string_ptr = two; break;
 case '3': string_ptr = three; break;
@@ -68,17 +60,15 @@ case '6': string_ptr = six; break;
 case '7': string_ptr = seven; break;
 case '8': string_ptr = eight; break;
 case '9': string_ptr = nine; break;
-default: continue; break;}                        
+case 0: break;} 
+if(!(digit))break;                       
 display_num_string(string_ptr, digit_num);
 digit_num++;
-_delay_ms(250);
-}while (digit_num < 8);
-                                                  //return to the top of the "do" loop until all digits 
-                                                  //have been illuminated
+_delay_us(1200);
+}  while (digit_num < 8); 
+if (UCSR0A & (1 << RXC0))break;
+}
 
-while(!(isCharavailable_Basic(1)))wdr(); 
-Char_from_PC_Basic();
-Clear_segments;Clear_digits; }                                       //clear display and repeat
 SW_reset;}
 
 
@@ -89,7 +79,7 @@ int char_ptr=0;                                                     //containing
 char letter;
 
 while(1){
-letter = *(s + char_ptr);// (s[char_ptr]);                          //Note these two expressions are equivalent
+letter = *(s + char_ptr);                                           //Note these two expressions are equivalent
 switch(letter){                                                     //Work through the segments contained in the 
 case 'a':                                                           //string until '\0' is encountered
 case 'b': 
