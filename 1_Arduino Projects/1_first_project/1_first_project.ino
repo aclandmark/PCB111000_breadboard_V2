494023b2af99a8ea258a2c23e1603642094b9f0e
aclandmark <mark.osborne15@btinternet.com>
2026-04-09 21:53:06 +0100

    Create 1_first_project.ino

---
+++ 1_Arduino Projects/1_first_project/1_first_project.ino
@@ -0,0 +1,36 @@
+
+
+//14 more projects very like those saved under            
+//“0_project_zero” this time generating simple patterns on the display 
+//some of them regular and repeating and some of them random.
+//Why not copy the C code for one or two examples to be found in folder "C_file_images\1_first_project"
+
+
+
+#include "First_project_header.h"
+
+
+#include "Local_subroutines.c"
+
+unsigned int PRN;
+unsigned char PRN_counter;
+
+ int main (void)                //Example 13
+  {int T1_period = 15;
+  char direction;
+  char seg_counter;
+  setup_HW;
+  
+  I2C_Tx_any_segment_clear_all();
+  PRN_counter = 0;
+  PRN = PRN_16bit_GEN (0, &PRN_counter);
+   direction=1;
+  while(1){
+     direction ^= 1;
+   seg_counter=0;
+ PRN = update_display
+ (T1_period, direction, seg_counter, PRN);
+ if(switch_1_down); 
+ else T1_period = Adjust_T1_period(T1_period);}}
+ 
+ 
