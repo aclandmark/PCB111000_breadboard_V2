7ebd11b882de8b5b129b731772d201018b89932d
aclandmark <mark.osborne15@btinternet.com>
2026-04-12 21:04:45 +0100

    Update Local_subroutines.c

--- 1_Arduino Projects/1_first_project/Local_subroutines.c
+++ 1_Arduino Projects/1_first_project/Local_subroutines.c
@@ -1,5 +1,8 @@
 
 #include "display_header.h"
+#include <util/delay.h>
+
+void display_pattern(void);
 
 char display_bkp[7];
 char seg_store[8];
@@ -37,6 +40,8 @@ unsigned long update_display (int T1_period, char direction, char seg_counter, u
 {char letter;
 char prompt;
 
+//for(int m = 0; m <= 7; m++)seg_store[m] = 0;
+
   while (seg_counter < 56) {
       letter = (PRN % 7) + 'a';
       PRN = PRN_16bit_GEN (PRN, &PRN_counter);
@@ -49,38 +54,46 @@ char prompt;
        PRN_counter -= 1;
        continue;}
        
-      seg_store[digit_num] |= (1 << (letter - 'a'));;
+      if(!(direction)) seg_store[digit_num] |= (1 << (letter - 'a'));
+      else seg_store[digit_num] &= (~(1 << (letter - 'a')));
       
+      
+      display_pattern();
       backup_the_display(letter, digit_num);
       seg_counter += 1;
-      Timer_T0_10mS_delay_x_m(T1_period);}
+            }
       return PRN;}
 
 
-void display_pattern(){     //repeat every 1.5ms but return to update display
+void display_pattern(void){     //repeat every 1.5ms but return to update display
 
+for (int q = 0; q <= 100; q++){
 for(int p = 0; p <= 7; p++){
 
-  switch (p){
-case 0: Clear_digits; digit_1_RH_on; break;
-case 1: Clear_digits; digit_2_RH_on; break;
-case 2: Clear_digits; digit_3_RH_on; break;
-case 3: Clear_digits; digit_4_RH_on; break;
-case 4: Clear_digits; digit_1_LH_on; break;
-case 5: Clear_digits; digit_2_LH_on; break;
-case 6: Clear_digits; digit_3_LH_on; break;
-case 7: Clear_digits; digit_4_LH_on; break;}
+Clear_segments;
+Clear_digits;
+  
+switch (p){
+case 0: digit_1_RH_on; break;
+case 1: digit_2_RH_on; break;
+case 2: digit_3_RH_on; break;
+case 3: digit_4_RH_on; break;
+case 4: digit_1_LH_on; break;
+case 5: digit_2_LH_on; break;
+case 6: digit_3_LH_on; break;
+case 7: digit_4_LH_on; break;}
 
 for(int m = 0; m <=7; m++){
-  if (seg_store[p] & (1 << m))switch(m){
+  if (seg_store[p] & (1 << m)){switch(m){
   case 0: a_on; break;
   case 1: b_on; break;
   case 2: c_on; break;
   case 3: d_on; break;
   case 4: e_on; break;
   case 5: f_on; break;
-  case 6: g_on; break;}
-}
+  case 6: g_on; break;}}
+}_delay_us(120);
 }}  
-
+Clear_segments;
+Clear_digits;}
     /*************************************************************************************************/
