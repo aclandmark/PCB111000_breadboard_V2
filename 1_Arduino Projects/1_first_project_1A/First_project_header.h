dc7203b97c6b54605f039d1f27cd5b439cb9e7be
aclandmark <mark.osborne15@btinternet.com>
2026-04-16 21:52:40 +0100

    Update First_project_header.h

--- 1_Arduino Projects/1_first_project_1A/First_project_header.h
+++ 1_Arduino Projects/1_first_project_1A/First_project_header.h
@@ -4,6 +4,8 @@
 
 char User_response;
 char watch_dog_reset;
+unsigned int PRN;
+unsigned char PRN_counter;
 
 #define T0_delay_10ms   5,178
 
@@ -13,6 +15,7 @@ ADMUX |= (1 << REFS0);\
 OSC_CAL;\
 Set_display_drivers;\
 Clear_segments;\
+Clear_digits;\
 setup_PC_comms_Basic(0,16);\
 Timer_T0_10mS_delay_x_m(5);
 
@@ -34,21 +37,16 @@ DDRC = (1 << DDC0) | (1 << DDC1) | (1 << DDC2) | (1 << DDC3);\
 DDRD = (1 << DDD2) |(1 << DDD3) |(1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7);
 
 
-#define set_up_switched_inputs \
-MCUCR &= (~(1 << PUD));\
-DDRD &= (~(1 << PD2));\
-PORTD |= (1 << PD2);
-
-#define switch_3_down ((PIND & 0x04)^0x04)
-
-
-
+/***********************************************************************************************/
 #define User_prompt_Basic \
 while(1){\
 do{String_to_PC_Basic("R?    ");}  while((isCharavailable_Basic (50) == 0));\
 User_response = Char_from_PC_Basic();\
 if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_Basic("\r\n");
 
+
+
+/***********************************************************************************************/
 #define OSC_CAL \
 if ((eeprom_read_byte((uint8_t*)0x1FE) > 0x0F)\
 &&  (eeprom_read_byte((uint8_t*)0x1FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x1FE)\
