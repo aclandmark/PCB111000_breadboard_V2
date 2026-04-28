77da2c52d9260a8666afe905aa2e24460ef5102d
aclandmark <mark.osborne15@btinternet.com>
2026-04-28 22:06:30 +0100

    Update 7B_calculator_based_clock.ino

--- 1_Arduino Projects/7B_calculator_based_clock/7B_calculator_based_clock.ino
+++ 1_Arduino Projects/7B_calculator_based_clock/7B_calculator_based_clock.ino
@@ -20,30 +20,36 @@
 
 
 #include "Proj_7B_header_file_1.h"
+#include "display_subroutines.c"
+
 
 //Type main routine here
+
+volatile int display_control = 0;
 
 
 int main (void){
 char User_response;
 
-setup_HW_Arduino_IO_Extra;
+setup_HW;
 
-Serial.write("Press 'R' to enter time or 'r' to start at time zero  ");
-User_prompt_A;
+String_to_PC_Basic("Press 'R' to enter time or 'r' to start at time zero  ");
+User_prompt_Basic;
 
 if(User_response == 'R')set_time();
 else {reset_clock_1; deci_SecsH = '0'; deci_SecsL = '0'; deci_sec_counter = 0; }
-I2C_Tx_8_byte_array(digits);
+display_8_bytes(digits);
 
-Serial.write("AK to start\r\n");
-waitforkeypress_A();
+String_to_PC_Basic("AK to start\r\n");
+waitforkeypress_Basic();
+sei();
+Timer_T1_sub_with_interrupt(T1_delay_200ms);
 
-while(1){Timer_T0_10mS_delay_x_m(20); deci_sec_counter += 2;
+while(1){while(!(display_control)); display_control = 0; deci_sec_counter += 2;
 if(deci_sec_counter == 864000)deci_sec_counter = 0;
-Format_time_for_display();I2C_Tx_8_byte_array(digits);}}
+Format_time_for_display();display_8_bytes(digits);}}
 
-
+ISR(TIMER1_OVF_vect){display_control = 1;}
 
 /**********************************************************************************************************************/
 void Format_time_for_display(void){
@@ -63,23 +69,23 @@ timer_utoa(deci_Secs * 10); deci_SecsH = charH; deci_SecsL = charL; }
 void set_time(void){
 
 for(int m = 0; m <= 7; m++)digits[m] = 0;
-Serial.write("Enter start time Hours, Minutes and Seconds\
+String_to_PC_Basic("Enter start time Hours, Minutes and Seconds\
 \r\n(24 hour clock with no spaces). Terminate with cr\r\n");
 
-while(isCharavailable_A(50) == 0){Serial.write("T?  ");}
+while(isCharavailable_Basic(50) == 0){String_to_PC_Basic("T?  ");}
 
-digits[7] = Serial.read();I2C_Tx_8_byte_array(digits);
-for (int m = 0; m<=4; m++){while(isCharavailable_A(5) == 0);
-if(m == 4){digits[2] = Serial.read();deci_SecsH = '0'; deci_SecsL = '0';}
-else digits[6 - m] = Serial.read(); 
-I2C_Tx_8_byte_array(digits);}
+digits[7] = Char_from_PC_Basic();display_8_bytes(digits);
+for (int m = 0; m<=4; m++){while(isCharavailable_Basic(5) == 0);
+if(m == 4){digits[2] = Char_from_PC_Basic();deci_SecsH = '0'; deci_SecsL = '0';}
+else digits[6 - m] = Char_from_PC_Basic(); 
+display_8_bytes(digits);}
 
-waitforkeypress_A();
+waitforkeypress_Basic();
 
 deci_sec_counter = 10*(long)((((long)((HoursH -'0') * 10) + HoursL-'0') * 3600) +
 ((((MinsH-'0') * 10) + MinsL-'0') * 60) +(SecsH-'0') * 10 + SecsL - '0');
 
-I2C_Tx_any_segment_clear_all();
+clear_display;
 _delay_ms(50);}
 
 
