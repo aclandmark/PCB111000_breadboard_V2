45fc2d0bf5044a26c07db9ddcbee6ef152344a68
aclandmark <mark.osborne15@btinternet.com>
2026-04-29 17:51:11 +0100

    Update 7B_calculator_based_clock.ino

--- 1_Arduino Projects/7B_calculator_based_clock/7B_calculator_based_clock.ino
+++ 1_Arduino Projects/7B_calculator_based_clock/7B_calculator_based_clock.ino
@@ -43,16 +43,18 @@ display_8_bytes(digits);
 String_to_PC_Basic("AK to start\r\n");
 waitforkeypress_Basic();
 sei();
-Timer_T1_sub_with_interrupt(T1_delay_200ms);
+Timer_T1_sub_with_interrupt(T1_delay_200ms);display_8_bytes(digits);}
 
-while(1){while(!(display_control)); display_control = 0; deci_sec_counter += 2;
+
+
+ISR(TIMER1_OVF_vect){TCNT1 = 0x9E62;//display_control = 1;
+deci_sec_counter += 2;
 if(deci_sec_counter == 864000)deci_sec_counter = 0;
-Format_time_for_display();display_8_bytes(digits);}}
 
-ISR(TIMER1_OVF_vect){display_control = 1;}
+
 
 /**********************************************************************************************************************/
-void Format_time_for_display(void){
+//void Format_time_for_display(void){
 Hours =     deci_sec_counter/36000;
 Minutes =   (deci_sec_counter%36000)/600;
 Seconds =   ((deci_sec_counter%36000)%600)/10;
@@ -74,16 +76,19 @@ String_to_PC_Basic("Enter start time Hours, Minutes and Seconds\
 
 while(isCharavailable_Basic(50) == 0){String_to_PC_Basic("T?  ");}
 
-digits[7] = Char_from_PC_Basic();display_8_bytes(digits);
+digits[7] = Char_from_PC_Basic();
+display_8_bytes(digits);
 for (int m = 0; m<=4; m++){while(isCharavailable_Basic(5) == 0);
-if(m == 4){digits[2] = Char_from_PC_Basic();deci_SecsH = '0'; deci_SecsL = '0';}
-else digits[6 - m] = Char_from_PC_Basic(); 
-display_8_bytes(digits);}
+if(m == 4){digits[2] = Char_from_PC_Basic();
+deci_SecsH = '0'; deci_SecsL = '0';}
+else digits[6 - m] = Char_from_PC_Basic();
+display_8_bytes(digits);
+}
 
 waitforkeypress_Basic();
 
-deci_sec_counter = 10*(long)((((long)((HoursH -'0') * 10) + HoursL-'0') * 3600) +
-((((MinsH-'0') * 10) + MinsL-'0') * 60) +(SecsH-'0') * 10 + SecsL - '0');
+deci_sec_counter = 10*(long)((((long)((HoursH - '0') * 10) + HoursL - '0') * 3600) +
+((((MinsH - '0') * 10) + MinsL - '0') * 60) +(SecsH - '0') * 10 + SecsL - '0');
 
 clear_display;
 _delay_ms(50);}
