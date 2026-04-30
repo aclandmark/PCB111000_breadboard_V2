e6a6b2dd4cff8de2c59bf74625df9dfcb8b1acd5
aclandmark <mark.osborne15@btinternet.com>
2026-04-30 17:57:43 +0100

    Update HW_timers.c

--- 1_Arduino Projects/Resources/Subroutines/HW_timers.c
+++ 1_Arduino Projects/Resources/Subroutines/HW_timers.c
@@ -13,8 +13,8 @@ void sendString(char*);
 /*********************************************************************/
 
 #define T0_delay_10ms 	5,178
-#define T1_delay_100ms 	3, 0x9E62
-#define T1_delay_200ms 	3, 0x3CC4
+#define T1_delay_200ms 	3, 0x9E62
+#define T1_delay_400ms 	3, 0x3CC4
 
 
 #define T1_delay_50ms 	5,0xFE78
