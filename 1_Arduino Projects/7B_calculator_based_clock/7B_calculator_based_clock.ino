b1ecb5cb3f4f71c9e3294f35269eb94da5e98b2c
aclandmark <mark.osborne15@btinternet.com>
2026-04-30 17:56:00 +0100

    Update 7B_calculator_based_clock.ino

--- 1_Arduino Projects/7B_calculator_based_clock/7B_calculator_based_clock.ino
+++ 1_Arduino Projects/7B_calculator_based_clock/7B_calculator_based_clock.ino
@@ -23,11 +23,6 @@
 #include "display_subroutines.c"
 
 
-//Type main routine here
-
-volatile int display_control = 0;
-
-
 int main (void){
 char User_response;
 
@@ -42,19 +37,20 @@ display_8_bytes(digits);
 
 String_to_PC_Basic("AK to start\r\n");
 waitforkeypress_Basic();
+UCSR0B &= (~(1 << RXEN0));
 sei();
-Timer_T1_sub_with_interrupt(T1_delay_200ms);display_8_bytes(digits);}
+Timer_T1_sub_with_interrupt(T1_delay_200ms);
+//start_clock_Local();
+display_8_bytes(digits);}
 
-
-
-ISR(TIMER1_OVF_vect){TCNT1 = 0x9E62;//display_control = 1;
+ISR(TIMER1_OVF_vect){TCNT1 = 0x9E62;
 deci_sec_counter += 2;
 if(deci_sec_counter == 864000)deci_sec_counter = 0;
-
+Format_time_for_display();}
 
 
 /**********************************************************************************************************************/
-//void Format_time_for_display(void){
+void Format_time_for_display(void){
 Hours =     deci_sec_counter/36000;
 Minutes =   (deci_sec_counter%36000)/600;
 Seconds =   ((deci_sec_counter%36000)%600)/10;
@@ -70,22 +66,21 @@ timer_utoa(deci_Secs * 10); deci_SecsH = charH; deci_SecsL = charL; }
 /***********************************************************************************************************************/
 void set_time(void){
 
-for(int m = 0; m <= 7; m++)digits[m] = 0;
+for(int m = 0; m <= 7; m++)digits[m] = 0; 
+
 String_to_PC_Basic("Enter start time Hours, Minutes and Seconds\
 \r\n(24 hour clock with no spaces). Terminate with cr\r\n");
-
+for(int m = 0; m <= 7; m++)digits[m] = 0;clear_display;
 while(isCharavailable_Basic(50) == 0){String_to_PC_Basic("T?  ");}
 
 digits[7] = Char_from_PC_Basic();
-display_8_bytes(digits);
+display_8_bytes(digits); 
+
 for (int m = 0; m<=4; m++){while(isCharavailable_Basic(5) == 0);
 if(m == 4){digits[2] = Char_from_PC_Basic();
 deci_SecsH = '0'; deci_SecsL = '0';}
-else digits[6 - m] = Char_from_PC_Basic();
-display_8_bytes(digits);
-}
-
-waitforkeypress_Basic();
+else {digits[6 - m] = Char_from_PC_Basic();
+display_8_bytes(digits);}}
 
 deci_sec_counter = 10*(long)((((long)((HoursH - '0') * 10) + HoursL - '0') * 3600) +
 ((((MinsH - '0') * 10) + MinsL - '0') * 60) +(SecsH - '0') * 10 + SecsL - '0');
@@ -108,3 +103,30 @@ else
 
 
 /**********************************************************************************/
+/*void initialise_T2_Local(void){
+ASSR = (1 << AS2); 
+TCNT2 = 0;
+TCCR2A = 0;
+TCCR2B |= (1 << CS20) | (1 << CS21);
+OCR2B = 0;}
+
+
+void start_clock_Local(void){
+tick_counter = 0;
+TCNT2 = 0;
+OCR2A = 102; 
+TIMSK2 |= (1 << OCIE2A);}
+
+
+ISR (TIMER2_COMPA_vect){ char string[5];
+  OCR2A += 102;
+  clock_tick += 1;
+  tick_counter += 1;
+  if(tick_counter == 9){tick_counter = -1; OCR2A += 4;}}
+
+
+
+ISR(TIMER1_OVF_vect){TCNT1 = 0x9E62;
+deci_sec_counter += 2;
+if(deci_sec_counter == 864000)deci_sec_counter = 0;
+Format_time_for_display();}*/
