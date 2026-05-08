8bfa2ff443511cfc19718c5bfa0d7fdf6332ce8c
aclandmark <mark.osborne15@btinternet.com>
2026-05-08 18:40:29 +0100

    Update dp_processor.h

--- 1_Arduino Projects/4_Display_Driver_Basic/RN_sq_root/dp_processor.h
+++ 1_Arduino Projects/4_Display_Driver_Basic/RN_sq_root/dp_processor.h
@@ -7,12 +7,18 @@ if (digits_pre_dp)\
 {for(int m = digits_pre_dp; m <= 7; m++)\
 {if (real_num_string[m])digits_post_dp+=1; else break;}}
 
+
+
+/************************************************************************************/
 #define add_trailing_zero_for_even_number_of_post_dp_digits \
 if(!(digits_post_dp%2)); else{\
 real_num_string[digits_post_dp + digits_pre_dp] = '0';\
 real_num_string[digits_post_dp + digits_pre_dp +1] = 0;\
 digits_post_dp += 1;}
 
+
+
+/************************************************************************************/
 #define integrate_dp_for_integer_root \
 /*String_to_PC_Basic("\r\n");*/\
 /*String_to_PC_Basic(digits);*/\
@@ -21,12 +27,17 @@ for(int m = 0; m <=14; m++)\
 digits[digits_pre_dp] |= 0x80;\
 for(int m = digits_pre_dp + 1; m < 14; m++)digits[m] = digits[m+1];
 
+
+
+/************************************************************************************/
 #define real_num_greater_than_1 \
 if (digits_pre_dp >= digits_post_dp)\
 {digits[digits_pre_dp - digits_post_dp]  |= 0x80;\
 digits[digits_pre_dp] &= ~0x80;}
 
 
+
+/************************************************************************************/
 #define real_num_less_than_1 \
 if (digits_post_dp > digits_pre_dp){\
 digits[digits_pre_dp] &= ~0x80;\
@@ -36,6 +47,8 @@ digits[digits_pre_dp] &= ~0x80;\
 digits[digits_pre_dp] &= ~0x80;digits[0] |= 0x80;}
 
 
+
+/************************************************************************************/
 #define diagnostic_print_out \
 String_to_PC_Basic("\r\n");Char_to_PC_Basic(digits_pre_dp + '0');\
 Char_to_PC_Basic('\t'); Char_to_PC_Basic(digits_post_dp + '0');\
