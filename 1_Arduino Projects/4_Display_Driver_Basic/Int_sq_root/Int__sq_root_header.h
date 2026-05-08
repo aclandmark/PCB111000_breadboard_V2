a920aabebf67ba35403d0d26551e780a9ecc3e34
aclandmark <mark.osborne15@btinternet.com>
2026-05-08 19:38:49 +0100

    Update Int__sq_root_header.h

--- 1_Arduino Projects/4_Display_Driver_Basic/Int_sq_root/Int__sq_root_header.h
+++ 1_Arduino Projects/4_Display_Driver_Basic/Int_sq_root/Int__sq_root_header.h
@@ -7,6 +7,9 @@ char watch_dog_reset;
 
 #define T0_delay_10ms   5,178
 
+
+
+/******************************************************************************/
 #define setup_HW \
 setup_watchdog;\
 ADMUX |= (1 << REFS0);\
@@ -17,6 +20,8 @@ setup_PC_comms_Basic(0,16);\
 Timer_T0_10mS_delay_x_m(5);
 
 
+
+/******************************************************************************/
 #define setup_watchdog \
 if (MCUSR & (1 << WDRF))watch_dog_reset = 1;\
 wdr();\
@@ -28,6 +33,9 @@ WDTCSR = 0;
 
 #define SW_reset {wdt_enable(WDTO_30MS);while(1);}
 
+
+
+/******************************************************************************/
 #define Set_display_drivers \
 DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4) | (1 << DDB5);\
 DDRC = (1 << DDC0) | (1 << DDC1) | (1 << DDC2) | (1 << DDC3);\
@@ -35,29 +43,26 @@ DDRD = (1 << DDD2) |(1 << DDD3) |(1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 <<
 
 
 
-
-
-
-
+/******************************************************************************/
 #define set_up_switched_inputs \
 MCUCR &= (~(1 << PUD));\
 DDRD &= (~(1 << PD2));\
 PORTD |= (1 << PD2);
 
-
-
 #define switch_3_down ((PIND & 0x04)^0x04)
 
 
 
-
-
+/******************************************************************************/
 #define User_prompt_Basic \
 while(1){\
 do{String_to_PC_Basic("R?    ");}  while((isCharavailable_Basic (50) == 0));\
 User_response = Char_from_PC_Basic();\
 if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_Basic("\r\n");
 
+
+
+/******************************************************************************/
 #define OSC_CAL \
 if ((eeprom_read_byte((uint8_t*)0x1FE) > 0x0F)\
 &&  (eeprom_read_byte((uint8_t*)0x1FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x1FE)\
@@ -69,3 +74,8 @@ if ((eeprom_read_byte((uint8_t*)0x1FE) > 0x0F)\
 /*****************************************************************************/
 #include "Resources/Subroutines/HW_timers.c"
 #include "Resources/PC_comms/Basic_Rx_Tx_Basic.c"
+
+
+
+
+/******************************************************************************/
