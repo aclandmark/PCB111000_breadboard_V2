adf116721c2fc943629bb40a36e30f9e6b5a2b8a
aclandmark <mark.osborne15@btinternet.com>
2026-06-19 21:37:59 +0100

    Update 1_Segment_driver.ino

--- 2_Arduino Projects Short display/4_Display_Driver_Basic/1_Segment_driver/1_Segment_driver.ino
+++ 2_Arduino Projects Short display/4_Display_Driver_Basic/1_Segment_driver/1_Segment_driver.ino
@@ -10,12 +10,53 @@
 
 #define message_1 "\r\nPress key a to g (x for next digit).\r\n\
 Press 'z' to clear the display.\r\n"
-#define message_2 "?\r\n"
+#define message_2 "Again ?\r\n"
 
 
-//Type main routine here
+int main (void){
+
+char letter = 0, digit_num;            
+char seg_counter = 0;
+setup_HW;
+//_delay_ms(1);
 
 
+if(first_run_after_programming)
+{clear_programmer;
+String_to_PC_Basic(message_1);}
+else String_to_PC_Basic(message_2);
+
+digit_num=1;
+while(digit_num <= 4){
+
+do{
+letter = waitforkeypress_Basic();
+  
+  if(!(seg_counter)){
+ Clear_digits;
+  Clear_segments;
+  
+switch (digit_num){
+  case 4: digit_4_LH_on; break;
+  case 3: digit_3_LH_on; break;
+  case 2: digit_2_LH_on; break;
+  case 1: digit_1_LH_on; break;}}
+  seg_counter += 1;
+  
+  switch(letter)
+ {case 'a': a_on;   break;                     
+case 'b': b_on;    break;                    
+case 'c': c_on;    break;
+case 'd': d_on;    break;
+case 'e': e_on;    break;
+case 'f': f_on;    break;
+case 'g': g_on;    break; 
+case 'x':  digit_num += 1;seg_counter = 0; break;
+case 'z': SW_reset;break;
+default: break;}
+
+}while(letter != 'x');
+}SW_reset; }
 
 
 
