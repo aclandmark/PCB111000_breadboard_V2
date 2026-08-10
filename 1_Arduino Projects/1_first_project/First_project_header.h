e9b4cc71c6e31846d4569c496add7fb54c6d6d44
aclandmark <mark.osborne15@btinternet.com>
2026-08-10 18:10:11 +0100

    Update First_project_header.h

--- 1_Arduino Projects/1_first_project/First_project_header.h
+++ 1_Arduino Projects/1_first_project/First_project_header.h
@@ -22,6 +22,8 @@ setup_PC_comms_Basic(0,16);\
 Timer_T0_10mS_delay_x_m(5);
 
 
+
+/***********************************************************************************************/
 #define setup_watchdog \
 if (MCUSR & (1 << WDRF))watch_dog_reset = 1;\
 wdr();\
@@ -35,6 +37,7 @@ WDTCSR = 0;
 
 
 
+/***********************************************************************************************/
 #define Set_display_drivers \
 DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4) | (1 << DDB5);\
 DDRC = (1 << DDC0) | (1 << DDC1) | (1 << DDC2) | (1 << DDC3);\
@@ -42,7 +45,7 @@ DDRD = (1 << DDD2) |(1 << DDD3) |(1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 <<
 
 
 
-/***************************************************************/
+/***********************************************************************************************/
 #define set_up_switched_inputs \
 MCUCR &= (~(1 << PUD));\
 DDRC &= (~((1 << PC5) | (1 << PC4)));\
@@ -51,33 +54,33 @@ PORTC |= ((1 << PC5) | (1 << PC4));
 
 
 /***********************************************************************************************/
-#define User_prompt_Basic \
+#define User_prompt_B \
 while(1){\
-do{String_to_PC_Basic("R?    ");}  while((isCharavailable_Basic (50) == 0));\
-User_response = Char_from_PC_Basic();\
-if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_Basic("\r\n");
+do{String_to_PC_B("R?    ");}  while((isCharavailable_B (50) == 0));\
+User_response = Char_from_PC_B();\
+if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_B("\r\n");
 
 
 
 /***********************************************************************************************/
 #define OSC_CAL \
-if ((eeprom_read_byte((uint8_t*)0x3FE) > 0x0F)\
-&&  (eeprom_read_byte((uint8_t*)0x3FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x3FE)\
-== eeprom_read_byte((uint8_t*)0x3FF))) {OSCCAL = eeprom_read_byte((uint8_t*)0x3FE);}
+if ((eeprom_read_byte((uint8_t*)0x1FE) > 0x0F)\
+&&  (eeprom_read_byte((uint8_t*)0x1FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x1FE)\
+== eeprom_read_byte((uint8_t*)0x1FF))) {OSCCAL = eeprom_read_byte((uint8_t*)0x1FE);}
 
 
 
 /********************************************************/
-#define first_run_after_programming   !(eeprom_read_byte((uint8_t*)0x3FA))
-#define clear_programmer              eeprom_write_byte((uint8_t*)0x3FA, 0xFF);
+#define first_run_after_programming   !(eeprom_read_byte((uint8_t*)0x1FA))
+#define clear_programmer              eeprom_write_byte((uint8_t*)0x1FA, 0xFF);
 
 
 
-/*****************************************************************************/
-#include "328_Resources/Subroutines/HW_timers.c"
-#include "328_Resources/PC_comms/Basic_Rx_Tx_Basic.c"
-#include "328_Resources/Subroutines/Random_and_prime_nos.c"
+/***********************************************************************************************/
+#include "168_Resources/Subroutines/HW_timers.c"
+#include "168_Resources/PC_comms/Basic_Rx_Tx_Basic.c"
+#include "168_Resources/Subroutines/Random_and_prime_nos.c"
 
 
 
-/********************************************************************************/
+/***********************************************************************************************/
