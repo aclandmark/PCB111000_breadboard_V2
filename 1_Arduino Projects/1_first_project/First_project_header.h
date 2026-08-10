ebd16abedfac5a8dd44d6d87ec90fdb8f3031db4
aclandmark <mark.osborne15@btinternet.com>
2026-08-10 17:58:08 +0100

    Update First_project_header.h

--- 1_Arduino Projects/1_first_project/First_project_header.h
+++ 1_Arduino Projects/1_first_project/First_project_header.h
@@ -1,9 +1,10 @@
-
+//#define F_CPU 8000000
 
 #include <avr/wdt.h>
 
 char User_response;
 char watch_dog_reset;
+//char power_on_reset;
 unsigned int PRN;
 unsigned char PRN_counter;
 
@@ -16,6 +17,7 @@ OSC_CAL;\
 Set_display_drivers;\
 Clear_segments;\
 Clear_digits;\
+set_up_switched_inputs;\
 setup_PC_comms_Basic(0,16);\
 Timer_T0_10mS_delay_x_m(5);
 
@@ -31,10 +33,21 @@ WDTCSR = 0;
 
 #define SW_reset {wdt_enable(WDTO_30MS);while(1);}
 
+
+
 #define Set_display_drivers \
 DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4) | (1 << DDB5);\
 DDRC = (1 << DDC0) | (1 << DDC1) | (1 << DDC2) | (1 << DDC3);\
 DDRD = (1 << DDD2) |(1 << DDD3) |(1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7);
+
+
+
+/***************************************************************/
+#define set_up_switched_inputs \
+MCUCR &= (~(1 << PUD));\
+DDRC &= (~((1 << PC5) | (1 << PC4)));\
+PORTC |= ((1 << PC5) | (1 << PC4));
+
 
 
 /***********************************************************************************************/
@@ -48,17 +61,22 @@ if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_Basic("
 
 /***********************************************************************************************/
 #define OSC_CAL \
-if ((eeprom_read_byte((uint8_t*)0x1FE) > 0x0F)\
-&&  (eeprom_read_byte((uint8_t*)0x1FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x1FE)\
-== eeprom_read_byte((uint8_t*)0x1FF))) {OSCCAL = eeprom_read_byte((uint8_t*)0x1FE);}
+if ((eeprom_read_byte((uint8_t*)0x3FE) > 0x0F)\
+&&  (eeprom_read_byte((uint8_t*)0x3FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x3FE)\
+== eeprom_read_byte((uint8_t*)0x3FF))) {OSCCAL = eeprom_read_byte((uint8_t*)0x3FE);}
 
+
+
+/********************************************************/
+#define first_run_after_programming   !(eeprom_read_byte((uint8_t*)0x3FA))
+#define clear_programmer              eeprom_write_byte((uint8_t*)0x3FA, 0xFF);
 
 
 
 /*****************************************************************************/
-#include "Resources/Subroutines/HW_timers.c"
-#include "Resources/PC_comms/Basic_Rx_Tx_Basic.c"
-#include "Resources/Subroutines/Random_and_prime_nos.c"
+#include "328_Resources/Subroutines/HW_timers.c"
+#include "328_Resources/PC_comms/Basic_Rx_Tx_Basic.c"
+#include "328_Resources/Subroutines/Random_and_prime_nos.c"
 
 
 
