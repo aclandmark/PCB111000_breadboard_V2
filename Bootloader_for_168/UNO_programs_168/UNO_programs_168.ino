eba3477fbd1bf332294f8ac906c0281cf32882bb
aclandmark <mark.osborne15@btinternet.com>
2026-08-13 22:30:11 +0100

    Update UNO_programs_168.ino

--- Bootloader_for_168/UNO_programs_168/UNO_programs_168.ino
+++ Bootloader_for_168/UNO_programs_168/UNO_programs_168.ino
@@ -70,6 +70,7 @@ Read_write_mem('I', EE_size - 5, \
 (Atmel_config(signature_bit_3_h, signature_bit_3_l)));       
 
 Read_write_mem('I', 0x1FC, 0xF3);                                          //Initialise EEP locations for PRN use
+Read_write_mem('I', 0x1EF, 0xFF);
 
 sendString("Set BR to 57600 then press AK\r\n");
 USART_init(0,16);
