022e3079bef689cc26365c5f2b3b621edd45b1e8
aclandmark <mark.osborne15@btinternet.com>
2026-08-19 07:48:53 +0100

    Update Atmega_168_bootloader.c

--- Bootloader_for_168/Atmega_168_bootloader/Atmega_168_bootloader/Atmega_168_bootloader.c
+++ Bootloader_for_168/Atmega_168_bootloader/Atmega_168_bootloader/Atmega_168_bootloader.c
@@ -53,7 +53,7 @@ char mode;													//'h' for hex file, 't' for text file
 
 int main (void){ 											//Loaded at address 0x3800, the start of the boot loader section
 
-if(eeprom_read_byte((uint8_t*)(0x1EF)))						//Set to 0xFF by verification and UNO programmer
+if((eeprom_read_byte((uint8_t*)(0x1EF))) & 1)					//Test bit zero (set by verification and UNO programmer)
 asm("jmp 0x3580");														//Jump to launcher which can set 0x1EF to zero
 
 		PageSZ = 0x40; PAmask = 0x1FC0;										//Define flash memory parameters for Atmega 168
