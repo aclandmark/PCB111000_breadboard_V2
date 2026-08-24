ff71fb508a8435bd7c26e8a228496b59ca577d22
aclandmark <mark.osborne15@btinternet.com>
2026-08-24 21:28:19 +0100

    Update Local_subroutines.c

--- 1_Arduino Projects/3_Receiver_Transmitter_Basic/2_Bin_to_String/Local_subroutines.c
+++ 1_Arduino Projects/3_Receiver_Transmitter_Basic/2_Bin_to_String/Local_subroutines.c
@@ -1,9 +1,9 @@
 
-void Char_to_PC_Basic(char);
+void Char_to_PC_B(char);
 
-void Int_to_PC_Basic (long);
-void newline_Basic(void);
-void String_to_PC_Basic(char*);
+void Int_to_PC_B (long);
+void newline_B(void);
+void String_to_PC_B(char*);
 
 
 
@@ -21,7 +21,7 @@ int Bin_to_string_Local(long number, char *s){
 
 void Num_string_to_PC_Local(char *s, int str_len){
  for (int m = str_len; m > 0; m--)
- Char_to_PC_Basic(s[m - 1]);}
+ Char_to_PC_B(s[m - 1]);}
 
 
 
@@ -31,7 +31,7 @@ void Num_string_to_PC_Local(char *s, int str_len){
 int max_num;
 int test_num, next_term=0;
 
-newline_Basic(); newline_Basic();
+newline_B(); newline_B();
 
   
 test_num = num_array[0];
@@ -45,13 +45,13 @@ test_num = max_num;
 for(int n = 0; n < array_length; n++)
 {if((num_array[n] <= test_num) && (num_array[n] > next_term)) 
 test_num = num_array[n];} 
-Int_to_PC_Basic(test_num);String_to_PC_Basic("  ");
+Int_to_PC_B(test_num);String_to_PC_B("  ");
 
 if(test_num == max_num)break;
 
 next_term = test_num;}
 
-newline_Basic(); newline_Basic();}
+newline_B(); newline_B();}
 
 
 
