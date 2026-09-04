918bee0848648fb6ed5c6f539c5d8b88257fd2cf
aclandmark <mark.osborne15@btinternet.com>
2026-09-04 20:38:30 +0100

    Update Hex_verification.c

--- Bootloader_for_168/3_Hex_verification/Hex_verification/Hex_verification.c
+++ Bootloader_for_168/3_Hex_verification/Hex_verification/Hex_verification.c
@@ -3,12 +3,12 @@
 Compile it using optimization level s ONLY
 Rx/Tx work at 57.6k
 
-File size is 0x06D2 bytes
-It is loaded at 0x2E80 (0x1740 HW address) and ends at 0x3552
-The launcher starts at 0x3580 leaving 46 unused bytes for expansion
+File size is 0x057E bytes
+It is loaded at 0x3000 (0x1800 HW address) and ends at 0x357E
+The launcher starts at 0x3580
 
-Space for user apps is 0x2E80 bytes		72.65%
-Space for bootloader is 0x1180 bytes	27.34%
+Space for user apps is 0x3000 (12288) bytes		75%	
+Space for bootloader is 0x1000 (4096) bytes		25%
 */
 
 
@@ -18,29 +18,23 @@ Space for bootloader is 0x1180 bytes	27.34%
 
 
 int main (void){
-	char digits[8];	
-
-	sendString("\t0 to verify or AOK\r\n");								//Press '0' to read out hex file from flash
+	char mode;	
 	
+	sendString("\r\nVerification: 0 to skip, 1 for full or 2 for basic\r\n");	
+		
 	Timer_T1_sub(T1_delay_200ms);
 	
-	UCSR0B |= (1 << RXEN0);
+	UCSR0B |= (1 << RXEN0);										//Turn receiver back on again
 	
-	if (!(waitforkeypress() - '0'))
-	{		
-		sendString("cmd_ctr? zero for full\t");
-		cmd_counter = (Int_from_PC(digits))/2;						///0x40;
-		if (!(cmd_counter))cmd_counter = FlashSZ;
-		Hex_to_PC (cmd_counter);
-		prog_counter = cmd_counter;
-			
-		Verify_Flash();
-		newline();
-		if(cmd_counter == FlashSZ)
-		{sendString("Hex_file_size:	0x");								//Confirm file sizes are all identical
-		Hex_to_PC(read_ops*2); }
+	mode = waitforkeypress() - '0';
+	if (mode)
+	{	prog_counter = FlashSZ;
+		Verify_Flash(mode - 1);
+	
+		sendString("Hex_file_size:	0x");								//Confirm file sizes are all identical
+		Hex_to_PC(read_ops*2); 
 
-	sendString("UNO Fuses E, H, L and lock:\t");						//Print out device configuration bytes
+	sendString("\r\nFuses E, H, L and lock:\t");						//Print out device configuration bytes
 	address_in_flash = 0;
 	for(int m = 0; m<4; m++){
 		switch(m){
@@ -52,38 +46,35 @@ int main (void){
 		Prog_mem_address_H = address_in_flash >> 8;
 		Prog_mem_address_L = address_in_flash;
 		read_config_bytes();
-		short_num_to_PC(Flash_readout);	sendChar('\t');
-	}}newline(); newline();
+		short_num_to_PC(Flash_readout);	sendChar('\t');	}}
+	
+	newline(); newline();											//Jump to here if verification not required
 
 eeprom_write_byte((uint8_t*)0x1EF,0b11111101);
 
 MCUSR = 0;
 	wdt_enable(WDTO_15MS);
-	while(1);											//Set WDTout reset flag.
+	while(1);														
 return 1;}
 
 
 
 /***************************************************************************************************************************************************/
-void Verify_Flash (void){
-	int  line_counter = 0, print_line;									//Controls printing of hex file
+void Verify_Flash (char print_mode){
+	int  line_counter = 0;												//Controls printing of hex file
 	int line_no;														//Refers to the .hex file
 	signed int phys_address;											//Address in flash memory
 	signed int prog_counter_mem;										//Initialized with size of .hex file used for programming
 	
 	phys_address = 0;  read_ops=0;
 	line_no = 0; prog_counter_mem = prog_counter;
-														//Start of verification routine
-
-sendString("\t0 or AOK\r\n");
-print_line = waitforkeypress() - '0';
 
 
 	while(1){if((prog_counter_mem <= 0))break;								//print out loop starts here, exit when finished
 		Hex_cmd = readCMD(phys_address);
 		phys_address++;
 		
-		if (print_line == 0){
+		if (print_mode){
 		if(!(line_no%10))sendChar('*');		
 		Timer_T0_sub(T0_delay_5ms);}
 				
@@ -100,12 +91,13 @@ print_line = waitforkeypress() - '0';
 			phys_address++;
 			if(Hex_cmd == 0xFFFF)break;									//Read 0xFFFF: return to start of print out loop
 			prog_counter_mem--;
-			if(print_line)
+			if(!(print_mode))
 			{Timer_T0_sub(T0_delay_5ms);								//5ms delay prevents PC from getting overwhelmed with characters
 			 Hex_to_PC (Hex_cmd);}
 			read_ops++;
 		if((phys_address >= FlashSZ))break;}
-		if ( (print_line != 0) && (!(line_counter%8)))sendString("\r\n");
+		if ( (!(print_mode)) && (!(line_counter%8)))sendString("\r\n");
+			
 		line_no++;
 	if ((phys_address >= FlashSZ) || (Hex_cmd == 0xFFFF))break;}
 newline(); }
