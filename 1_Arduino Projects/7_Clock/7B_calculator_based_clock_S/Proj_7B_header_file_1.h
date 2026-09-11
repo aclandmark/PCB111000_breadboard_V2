978e584e59ed5029584df8506285d2ab7b9cf3a9
aclandmark <mark.osborne15@btinternet.com>
2026-09-11 17:19:08 +0100

    Create Proj_7B_header_file_1.h

---
+++ 1_Arduino Projects/7_Clock/7B_calculator_based_clock_S/Proj_7B_header_file_1.h
@@ -0,0 +1,98 @@
+
+
+#include <avr/wdt.h>
+#include <util/delay.h>
+
+
+char watch_dog_reset = 0;
+char power_on_reset = 0;
+char User_response;
+char num_as_string[12];
+char str_counter;
+
+char digits[8], charH, charL ;
+char Hours, Minutes, Seconds, deci_Secs;
+volatile long deci_sec_counter;
+
+volatile char tick_counter; 
+volatile char clock_tick;
+volatile char dp;
+volatile char dp_digit;
+
+#define T0_delay_10ms   5,178
+
+#define setup_HW \
+setup_watchdog;\
+ADMUX |= (1 << REFS0);\
+OSC_CAL;\
+Set_display_drivers;\
+Clear_segments;\
+setup_PC_comms_B(0,16);\
+Timer_T0_10mS_delay_x_m(5);
+
+
+
+/***********************************************************************************/
+#define setup_watchdog \
+if (MCUSR & (1 << PORF))power_on_reset = 1;\
+if (MCUSR & (1 << WDRF))watch_dog_reset = 1;\
+wdr();\
+MCUSR &= ~(1<<WDRF);\
+WDTCSR |= (1 <<WDCE) | (1<< WDE);\
+WDTCSR = 0;
+
+#define wdr()  __asm__ __volatile__("wdr")
+
+#define SW_reset {wdt_enable(WDTO_30MS);while(1);}
+
+
+
+/***********************************************************************************/
+#define Set_display_drivers \
+DDRB = (1 << DDB0) | (1 << DDB1) | (1 << DDB2) | (1 << DDB3) | (1 << DDB4) | (1 << DDB5);\
+DDRC = (1 << DDC0) | (1 << DDC1) | (1 << DDC2) | (1 << DDC3);\
+DDRD = (1 << DDD2) |(1 << DDD3) |(1 << DDD4) | (1 << DDD5) | (1 << DDD6) | (1 << DDD7);
+
+#define reset_clock_1   {digits[0] = 0; for (int m = 1; m < 8; m++)digits[m] = '0'; }
+
+
+
+/***********************************************************************************/
+#define HoursH digits[3]
+#define HoursL digits[2]
+#define MinsH digits[1]
+#define MinsL digits[0]
+
+
+
+/***********************************************************************************/
+#define set_up_switched_inputs \
+MCUCR &= (~(1 << PUD));\
+DDRD &= (~(1 << PD2));\
+PORTD |= (1 << PD2);
+
+#define switch_3_down ((PINC & 0x20)^0x20)
+
+
+
+/***********************************************************************************/
+#define User_prompt_B \
+while(1){\
+do{String_to_PC_B("R?    ");}  while((isCharavailable_B (50) == 0));\
+User_response = Char_from_PC_B();\
+if((User_response == 'R') || (User_response == 'r'))break;} String_to_PC_B("\r\n");
+
+
+
+/***********************************************************************************/
+#define OSC_CAL \
+if ((eeprom_read_byte((uint8_t*)0x1FE) > 0x0F)\
+&&  (eeprom_read_byte((uint8_t*)0x1FE) < 0xF0) && (eeprom_read_byte((uint8_t*)0x1FE)\
+== eeprom_read_byte((uint8_t*)0x1FF))) {OSCCAL = eeprom_read_byte((uint8_t*)0x1FE);}
+
+
+
+
+/*****************************************************************************/
+#include "168_Resources/Subroutines/HW_timers.c"
+#include "168_Resources/PC_comms/Basic_Rx_Tx_Basic.c"
